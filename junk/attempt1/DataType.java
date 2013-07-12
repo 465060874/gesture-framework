a296ebaf1b1fd95bd6e4f3c14cf990cd8a4b89e1
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2013-07-12 16:34:00 +0100

    added gitignore file

--- junk/attempt1/DataType.java
+++
@@ -1,9 +0,0 @@
-package attempt1;
-
-/**
- * User: Sam Wright
- * Date: 20/06/2013
- * Time: 05:53
- */
-public interface DataType {
-}
---
+++ src/test/java/BestSoFar/framework/core/helper/ChildrenManagerTest.java
@@ -0,0 +1,44 @@
+package BestSoFar.framework.core.helper;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * User: Sam Wright Date: 12/07/2013 Time: 13:01
+ */
+public class ChildrenManagerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testWithChildren() throws Exception {
+
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+
+    }
+
+    @Test
+    public void testFixAsVersion() throws Exception {
+
+    }
+
+    @Test
+    public void testDiscardNext() throws Exception {
+
+    }
+
+    @Test
+    public void testDiscardPrevious() throws Exception {
+
+    }
+
+    @Test
+    public void testGetChildren() throws Exception {
+
+    }
+}
