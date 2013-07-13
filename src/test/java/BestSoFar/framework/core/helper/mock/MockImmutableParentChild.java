7fbf3c135f6e0643f7ae191b70100094de4774c3
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2013-07-13 10:54:53 +0100

    Fiddling with pom.xml to push javadocs to github pages...

--- src/test/java/BestSoFar/framework/core/helper/mock/MockImmutableParentChild.java
+++ src/test/java/BestSoFar/framework/core/helper/mock/MockImmutableParentChild.java
@@ -2,11 +2,13 @@ package BestSoFar.framework.core.helper.mock;
 
 import BestSoFar.framework.core.common.ChildOf;
 import BestSoFar.framework.core.common.ParentOf;
-import javafx.scene.Parent;
 
 /**
  * User: Sam Wright Date: 12/07/2013 Time: 13:11
  */
-public class MockImmutableParentChild extends MockEventuallyImmutable
-        implements ChildOf<MockImmutableParentChild>, ParentOf<MockImmutableParentChild> {
+public class MockImmutableParentChild {
+//    extends MockEventuallyImmutable
+//        implements ChildOf<MockImmutableParentChild>, ParentOf<MockImmutableParentChild> {
+
+
 }
