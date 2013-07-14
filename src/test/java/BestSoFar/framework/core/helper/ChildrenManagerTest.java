4bbbb5230846bec5dd3306e5f0b87b5652948c32
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2013-07-14 13:07:19 +0100

    Made mock child/parent immutable class for testing...

--- src/test/java/BestSoFar/framework/core/helper/ChildrenManagerTest.java
+++ src/test/java/BestSoFar/framework/core/helper/ChildrenManagerTest.java
@@ -1,5 +1,6 @@
 package BestSoFar.framework.core.helper;
 
+import BestSoFar.framework.core.helper.mock.MockImmutableParentChild;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,9 +8,11 @@ import org.junit.Test;
  * User: Sam Wright Date: 12/07/2013 Time: 13:01
  */
 public class ChildrenManagerTest {
+    private MockImmutableParentChild parentChild;
+
     @Before
     public void setUp() throws Exception {
-
+        parentChild = new MockImmutableParentChild();
     }
 
     @Test
