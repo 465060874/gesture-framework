491ea58becc9e1ecac63174133102473d6ddf973
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2013-07-14 12:29:59 +0100

    removed clutter from pom.xml

--- src/test/java/BestSoFar/framework/core/helper/mock/MockImmutableParentChild.java
+++ src/test/java/BestSoFar/framework/core/helper/mock/MockImmutableParentChild.java
@@ -6,8 +6,10 @@ import BestSoFar.framework.core.common.ParentOf;
 /**
  * User: Sam Wright Date: 12/07/2013 Time: 13:11
  */
-public class MockImmutableParentChild extends MockEventuallyImmutable
-        implements ChildOf<MockImmutableParentChild>, ParentOf<MockImmutableParentChild> {
+public class MockImmutableParentChild {
+//    extends
+//} MockEventuallyImmutable
+//        implements ChildOf<MockImmutableParentChild>, ParentOf<MockImmutableParentChild> {
 
 
 }
